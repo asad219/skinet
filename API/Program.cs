dc8a5194021d64b34e476e7e84ee597b5f8def81
Asad Ullah Khan <asad219@hotmail.com>
2023-10-13 11:36:30 +0400

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,12 @@
 
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -13,6 +15,9 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<StoreContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 
 var app = builder.Build();
 
@@ -28,5 +33,17 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+
+try{
+    await context.Database.MigrateAsync();
+    //await StoreContextSeed.SeedAsync(context);
+}catch(Exception ex){
+    logger.LogError(ex, "An error occoured during migration");
+}
 
 app.Run();
